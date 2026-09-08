3938bf878ce375688118c33ce0dea49e9ad39bb5
islammdsohan603 <islammdsohan603@gmail.com>
2026-09-08 22:55:21 +0600

    incrementByAmount add

--- src/redux/features/counter/Counter.jsx
+++ src/redux/features/counter/Counter.jsx
@@ -1,17 +1,23 @@
-
-import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { decrement,increment,reset } from "./createSlice";
+import { decrement, increment, reset, incrementByAmount } from "./createSlice";
 
-export function Counter(){
-    const count = useSelector((state)=>state.counter.value);
-    const dispatch=useDispatch();
-    return (
-        <div>
-            <h1>{count}</h1>
-            <button onClick={()=>dispatch(increment())}>Increment</button>
-            <button onClick={()=>dispatch(decrement())}>Decrement</button>
-            <button onClick={()=>dispatch(reset())}>Reset</button>
-        </div>
-    );
+export function Counter() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(5))}>
+        Increment by 5
+      </button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
+
+      <input
+        type="number"
+        onChange={(e) => dispatch(incrementByAmount(Number(e.target.value)))}
+      />
+    </div>
+  );
 }
